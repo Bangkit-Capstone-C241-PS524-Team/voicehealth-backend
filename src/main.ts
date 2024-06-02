2b16fee0d6067abcd51b655c3398132547cc5c77
unknown <wisnuyeahfaizal@gmail.com>
2024-06-03 03:15:49 +0700

    feat: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import {
+    DocumentBuilder,
+    SwaggerCustomOptions,
+    SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    app.setGlobalPrefix('api');
+    app.enableCors();
+
+    const config = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('Api Docs')
+        .setDescription('Api Documentation for VoiceHealth Guide')
+        .setVersion('1.0')
+        .build();
+
+    const option: SwaggerCustomOptions = {
+        swaggerOptions: {
+            docExpanstion: 'none',
+        },
+    };
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, document, option);
+
+    await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
