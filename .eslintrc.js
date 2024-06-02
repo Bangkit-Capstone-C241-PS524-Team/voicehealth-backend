088afe69b30794395b84122983369f1e98cf487d
unknown <wisnuyeahfaizal@gmail.com>
2024-06-02 15:38:22 +0700

    feat: folder structure & prisma setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+    ignorePatterns: ['**/docker/*', '**/dist/*'],
+    rules: {
+        'no-unused-vars': 'error',
+        'no-console': 'off',
+        'react/display-name': 'off',
+        'simple-import-sort/imports': 'warn',
+        'simple-import-sort/exports': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
+    },
+    globals: {
+        React: true,
+        JSX: true,
+    },
+    root: true,
 };
