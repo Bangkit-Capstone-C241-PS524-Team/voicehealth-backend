b06f0cbdd4afb41ef9d4f910e7ae201255815c06
unknown <wisnuyeahfaizal@gmail.com>
2024-06-05 13:23:07 +0700

    feat: response interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '@/modules/auth/auth.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseInterceptor } from '@/common/interceptors/response.interceptor';
 
 @Module({
     imports: [AuthModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ResponseInterceptor,
+        },
+    ],
 })
 export class AppModule {}
